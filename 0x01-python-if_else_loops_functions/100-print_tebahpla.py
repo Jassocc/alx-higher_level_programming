a9941fa8315021127484d6cfac53ea23ca369fbf
Jassocc <clnearle@gmail.com>
2023-09-05 06:39:33 -0700

    task 12

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
-for char in range(ord('z'), ord('a') - 1, - 1):
-    print("{}".format(chr(char)), end='')
+for char in range(0, 26):
+    u = ord('z') - char
+    if char % 2 == 1:
+        u = (chr(u - ord('a') + ord('A')))
+    else:
+        u = chr(u)
+    print("{}".format(u), end='')
