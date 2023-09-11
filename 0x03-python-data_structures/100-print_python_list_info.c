4179a6f5f60de82e99858c33d284c215cec4c88c
Jassocc <clnearle@gmail.com>
2023-09-11 00:41:54 -0700

    task 100

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,3 +1,20 @@
 #include <Python.h>
 
+/**
+ * print_python_list_info - prints info on pythonlists
+ * @p: object
+ */
+void print_python_list_info(PyObject *p)
+{
+	Py_ssize_t siz, i;
+	PyObject *it;
 
+	siz = PyList_Size(p);
+	printf("[*] Size of the Python List = %ld\n", siz);
+	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+	for (i = 0; i < siz; i++)
+	{
+		it = PyList_GetItem(p, i);
+		printf("Element %ld: %s\n", i, Py_TYPE(it)->tp_name);
+	}
+}
