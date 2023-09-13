ec871b90e9c6e3c5636a482f1b658a591b20ca03
Jassocc <clnearle@gmail.com>
2023-09-13 05:57:12 -0700

    task 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_direc = {}
-    for k, v in a_dictionary.items():
-        new_direc[key] = v * 2
-    return new_direc
+    if a_directory is not None:
+        new_direc = {}
+        for k, v in a_dictionary.items():
+            new_direc[key] = v * 2
+        return new_direc
+    return None
