0ddea934a608df01c49d9fbf19a799dee35ef8d0
Jassocc <clnearle@gmail.com>
2023-09-24 08:15:50 -0700

    task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    pri = 0
+    try:
+        for a in range(x):
+            if isinstance(my_list[a], int):
+                print("{:d}".format(my_list[a]), end="")
+                pri += 1
+    except (TypeError, ValueError):
+        pass
+    finally:
+        print()
+    return pri
