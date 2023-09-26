711d56142d9fd7fb1bf2810a2417e379137d76ab
Jassocc <clnearle@gmail.com>
2023-09-26 07:33:19 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module 1-square
+def of class square
+"""
+
+
+class Square:
+    """
+    Square def
+    Size of a square
+    """
+    def __init__(self, size):
+        """
+        initialize class square
+        size = size on the side of a square
+        """
+        self.__size = size
