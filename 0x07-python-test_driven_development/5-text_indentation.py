7bb0c5b6d71b111991e181801308f1178fd7fc50
Jassocc <clnearle@gmail.com>
2023-09-29 10:41:51 -0700

    task 5

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+module 5 text_indentation
+prints a text with 2 new lines after chars
+function:
+    def text_indentation(text):
+"""
+
+
+def text_indentation(text):
+    """
+    prints a text with new lines after
+    specified chars: . ? :
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    chars_split = ['.', '?', ':']
+    li = []
+    line = ''
+    for char in text:
+        line +=char
+        if char in chars_split:
+            li.append(line.strip())
+            line = ''
+    if line:
+        li.append(line.strip())
+    if not text.endswith(tuple(chars_split)):
+        print("\n\n".join(li), end="")
+    else:
+        print("\n\n".join(li))
