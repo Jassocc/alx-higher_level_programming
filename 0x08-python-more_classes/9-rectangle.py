84f34adef87590c063818a5ec941daef50c16f1e
Jassocc <clnearle@gmail.com>
2023-10-02 14:12:37 -0700

    task 9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -115,5 +115,4 @@ class Rectangle:
         """
         returns a new Rectangle instance with width == height == size
         """
-        rev = cls(size, size)
-        return rev
+        return cls(size, size)
