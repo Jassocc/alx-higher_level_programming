3a4ecb27881e9fb5a3764046cc2ba6c2c01d964d
Jassocc <clnearle@gmail.com>
2023-10-02 13:45:30 -0700

    task 9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -18,7 +18,7 @@ class Rectangle:
         initializer of class rectangle
         """
         self.__width = width
-        self.height = height
+        self.__height = height
         Rectangle.number_of_instances += 1
 
     @property
