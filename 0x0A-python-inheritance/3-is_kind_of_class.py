7e1f59642f1d8934c112657c6ee307a6d44d041d
Jassocc <clnearle@gmail.com>
2023-10-09 13:10:52 +0000

    task 3

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -4,6 +4,8 @@ Module 3-is_kind_of_class
 
 Returns true if instances are a match false if otherwise
 """
+
+
 def is_kind_of_class(obj, a_class):
     """
     checks if the instances are the sdame
