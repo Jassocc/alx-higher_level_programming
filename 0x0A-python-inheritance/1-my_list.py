af2f6f2ebf586019fa857fc049eec65313bba1c2
Jassocc <clnearle@gmail.com>
2023-10-09 12:27:08 +0000

    task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module 1-my_list
+
+a class that inherits from class lists
+"""
+
+
+class MyList(list):
+    """
+    class that inherits all the properties
+    from the class known as list
+    """
+    def print_sorted(self):
+        """
+        function that prints the list in
+        asc order
+        """
+        a = (sorted(self))
+        print(a)
