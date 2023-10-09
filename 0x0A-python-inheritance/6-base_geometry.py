e8650b808fdf79410cd9a0b55a87140585b2a7ed
Jassocc <clnearle@gmail.com>
2023-10-09 13:26:27 +0000

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Class GEometry based on prev task
+
+It should raise an exception
+"""
+
+class BaseGeometry:
+    """
+    class for area
+    """
+
+    def area(self):
+        """
+        function to raise the exception error
+        """
+        raise Exception("area() is not implemented")
