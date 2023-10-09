52c2e8f917ca8220630bf58885dff57cf1bb07bd
Jassocc <clnearle@gmail.com>
2023-10-09 12:43:29 +0000

    task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -11,6 +11,9 @@ class MyList(list):
     class that inherits all the properties
     from the class known as list
     """
+    def __init__(self, *args):
+        super().__init__(args)
+
     def print_sorted(self):
         """
         function that prints the list in
