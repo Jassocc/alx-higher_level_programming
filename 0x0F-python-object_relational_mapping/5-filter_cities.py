3620635eb3283980ae2f4e8176adeb97b2444d52
Colin <clnearle@gmail.com>
2023-12-15 05:02:17 -0800

    task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -10,12 +10,12 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          password=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT cities.id, cities.name FROM cities \
-                   JOIN states ON cities.state_id = states.id \
-                   WHERE states.name = %s \
+    cursor.execute("SELECT cities.name FROM states \
+                   INNER JOIN cities ON states.id = cities.state_id \
+                   WHERE states.name LIKE %s \
                    ORDER BY cities.id ASC", (sys.argv[4],))
     rows = cursor.fetchall()
-    for row in rows:
-        print(row)
+    city_names = (row[0] for row in rows)
+    print(', '.join("{:s}".format(name) for name in city_names))
     cursor.close()
     db.close()
