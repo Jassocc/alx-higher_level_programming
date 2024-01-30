c990ffa15c71db0119ee0c64f79df030d45b7188
Jassocc <clnearle@gmail.com>
2024-01-30 04:04:49 -0800

    task 8

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+const request = require('request');
+const movieId = process.argv[2];
+const apiURL = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+request.get(apiURL, (error, response, body) => {
+  if (error) {
+    console.error(`Error: ${error}`);
+    process.exit(1);
+  }
+  try {
+    const movieData = JSON.parse(body);
+    const charactersURLs = movieData.characters;
+    const fetchAndPrintCharacter = (characterURL) => {
+      request.get(characterURL, (charError, charResponse, charBody) => {
+        if (charError) {
+          console.error(`Error fetching character: ${charError}`);
+        } else {
+          const characterData = JSON.parse(charBody);
+          console.log(characterData.name);
+        }
+      });
+    };
+    charactersURLs.forEach(fetchAndPrintCharacter);
+  } catch (parseError) {
+    console.error('Error');
+    process.exit(1);
+  }
+});
