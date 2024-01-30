897f72700e717cb9ac13e068f058e4f5c18074db
Jassocc <clnearle@gmail.com>
2024-01-30 04:43:04 -0800

    task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,17 +1,15 @@
 #!/usr/bin/node
 const request = require('request');
 const apiURL = process.argv[2];
+const characterId = 18;
 request.get(apiURL, (error, response, body) => {
   if (error) {
     console.error(`Error: ${error}`);
     process.exit(1);
   }
-  const filmsData = JSON.parse(body);
-  let wedgeMovies = 0;
-  for (const film of filmsData.results) {
-    if (film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
-      wedgeMovies++;
-    }
-  }
-  console.log(wedgeMovies);
+  const filmsData = JSON.parse(body).results;
+  const numberOfMovies = filmsData.filter((film) =>
+    film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
+  ).length;
+  console.log(numberOfMovies);
 });
